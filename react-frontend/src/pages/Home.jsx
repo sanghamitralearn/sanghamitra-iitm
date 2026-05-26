1ff1468ea87603b3c88262ff635b856646f78350
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2026-05-26 23:30:40 +0530

    Update Home.jsx

--- react-frontend/src/pages/Home.jsx
+++ react-frontend/src/pages/Home.jsx
@@ -275,7 +275,6 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-              </div>
             </div>
 
             {/* JEE Advanced Course Card */}
