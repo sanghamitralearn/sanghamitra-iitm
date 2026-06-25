e3340141dd9c265f41ba31f47fd141bc903cdbb5
sanghamitralearn <sanghamitra.learn@gmail.com>
2026-06-25 10:32:10 +0530

    Update App.jsx

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -197,7 +197,7 @@ function App() {
             <Route path="/courses/sat/full-test" element={<SATFullTest />} />
             <Route path="/courses/sat/analysis" element={<SATAnalysis />} />
             <Route path="/admin/sat" element={<SATDashboard />} />
-            <Route path="/admin/sat/exam/:email/:subject/:attemptNumber" element={<SATExamAnalysis />} />
+            
           
 
             {/* DBMS COURSE */}
