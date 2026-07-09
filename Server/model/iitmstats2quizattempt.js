abebe047ae2191b77fb6014c7e9dae550f6dbe8b
sanghamitralearn <sanghamitra.learn@gmail.com>
2026-07-09 21:29:50 +0530

    Update iitmstats2quizattempt.js

--- Server/model/iitmstats2quizattempt.js
+++ Server/model/iitmstats2quizattempt.js
@@ -1,21 +1,17 @@
 const mongoose = require('mongoose');
 
-
 const quizAttemptSchema = new mongoose.Schema({
-
 
   email:    { type: String, default: '' },
   username: { type: String, default: '' },
   week:     { type: Number, default: 0, index: true },
   topic:    { type: String, default: '' },
 
-
   score:              { type: Number, default: 0 },
   max_possible_score: { type: Number, default: 0 },
   percentage:         { type: Number, default: 0 },
   total_questions:    { type: Number, default: 0 },
   correct_answers:    { type: Number, default: 0 },
-
 
   easy_attempted:   { type: Number, default: 0 },
   easy_correct:     { type: Number, default: 0 },
@@ -24,23 +20,18 @@ const quizAttemptSchema = new mongoose.Schema({
   hard_attempted:   { type: Number, default: 0 },
   hard_correct:     { type: Number, default: 0 },
 
-
   total_time_seconds: { type: Number, default: 0 },
   started_at:         { type: Date,   default: Date.now },
   submitted_at:       { type: Date,   default: Date.now },
 
-
   is_completed: { type: Boolean, default: true },
   cheat_count:  { type: Number,  default: 0 }
 
-
 }, { timestamps: true });
-
 
 quizAttemptSchema.index({ email: 1, week: 1 });
 quizAttemptSchema.index({ email: 1, submitted_at: -1 });
 quizAttemptSchema.index({ week: 1, percentage: -1 });
-
 
 module.exports = mongoose.model(
   'iitmstats2quizattempt',
