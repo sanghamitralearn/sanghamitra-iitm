ac1a609e4abd6668e2257d574e0442c0ec39f532
sanghamitralearn <sanghamitra.learn@gmail.com>
2026-09-01 10:40:13 +0530

    Update auth.js

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -752,8 +752,8 @@ router.get('/iitm_stats2_questions_databases', async (req, res) => {
 
 
    const weekNum = parseInt(week);
-    if (isNaN(weekNum) || ((weekNum < 1 || weekNum > 12) && weekNum !== 100 && weekNum !== 101)) {
-      return res.status(400).json({ error: 'week must be between 1 and 12, or 100/101 for the Midterm Assessments' });
+    if (isNaN(weekNum) || ((weekNum < 1 || weekNum > 12) && weekNum !== 100 && weekNum !== 101 && weekNum!== 102)) {
+      return res.status(400).json({ error: 'week must be between 1 and 12, or 100/101/102 for the Midterm Assessments' });
     }
 
 
